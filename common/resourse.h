aa9259f23d6919099ea3591d23182ec402232aca
bettermanW <1441916502@qq.com>
2025-08-22 17:01:59 +0800

    feat(resourse) 添加中、小字体模和冒号

---
+++ common/resourse.h
@@ -0,0 +1,13 @@
+//
+// Created by 14419 on 25-8-22.
+//
+
+#ifndef RESOURSE_H
+#define RESOURSE_H
+
+#include <stdint.h>
+extern const uint8_t small2Font[][22];
+extern const uint8_t midFont[][57];
+extern const uint8_t colon[];
+
+#endif //RESOURSE_H
