000bb6b12867e4f17b6db7e3d254b64541378548
bettermanW <1441916502@qq.com>
2025-08-31 20:55:51 +0800

    feat(game1)添加第一个游戏打砖块

---
+++ apps/Inc/game1.h
@@ -0,0 +1,17 @@
+//
+// Created by 14419 on 25-8-31.
+//
+
+#ifndef GAME1_H
+#define GAME1_H
+#include "menu.h"
+#include "config.h"
+#include "buzzer.h"
+
+#if COMPILE_GAME1
+
+
+void game1_start(void);
+
+#endif
+#endif //GAME1_H
